5547b35a03c6047bc7e84531a7deda501e5cd232
陳志洋 <andy@Andys-MacBook-Air.local>
2023-03-30 17:28:44 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+// import HelloController 
+//   from "./controllers/hello-controller.js"
+import TuitsController
+  from "./controllers/tuits/tuits-controller.js";
+import UserController
+  from "./controllers/users/users-controller.js"
+const app = express()
+app.use(cors())
+app.use(express.json());
+// HelloController(app)
+TuitsController(app);
+UserController(app)
+app.listen(process.env.PORT || 4000);
+
